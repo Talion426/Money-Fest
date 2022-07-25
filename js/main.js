8182adea37e9f72a8eb0f88fef25dfdde372b7a6
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-07-25 20:33:18 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    speed: 600,
+});
